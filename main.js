44068330878a6f2609dec346e78b1940ebcf36d8
Michele Cofano <brutalmachinelol@gmail.com>
2021-01-28 00:13:13 +0100

    create funzioni per scorrere prev e next

--- main.js
+++ main.js
@@ -7,9 +7,15 @@ new Vue({
       'https://static.photocdn.pt/images/articles/2017/04/28/iStock-646511634.jpg',
       'https://cdn.mos.cms.futurecdn.net/FUE7XiFApEqWZQ85wYcAfM.jpg',
       'https://static.photocdn.pt/images/articles/2017/04/28/iStock-546424192.jpg'
-    ]
+    ],
+    imgIDX: 0,
   },
   methods: {
-
+    next: function() {
+      this.imgIDX += 1;
+    },
+    prev: function() {
+      this.imgIDX -= 1;
+    }
   }
 });
