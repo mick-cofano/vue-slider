d9fe56d464054a4c1cd63242da582f4e1d37db71
Michele Cofano <brutalmachinelol@gmail.com>
2021-01-27 16:12:46 +0100

    Trasportate link immagini da html a VueJS

--- main.js
+++ main.js
@@ -1,7 +1,13 @@
 new Vue({
   el: '#root',
   data: {
-
+    imgs: [
+      'https://images.pexels.com/photos/371633/pexels-photo-371633.jpeg?cs=srgb&dl=clouds-country-daylight-371633.jpg&fm=jpg',
+      'https://static.photocdn.pt/images/articles/2017/04/28/iStock-646511634.jpg',
+      'https://static.photocdn.pt/images/articles/2017/04/28/iStock-646511634.jpg',
+      'https://cdn.mos.cms.futurecdn.net/FUE7XiFApEqWZQ85wYcAfM.jpg',
+      'https://static.photocdn.pt/images/articles/2017/04/28/iStock-546424192.jpg'
+    ]
   },
   methods: {
 
